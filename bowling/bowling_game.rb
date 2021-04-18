b79aaaf3d12ec6c0cf11306ea469c26d1e25b68a
Ofir Petrushka <hatkyinc@gmail.com>
2021-04-18 14:53:03 +1000

    Implement simple score for 1 frame

--- bowling/bowling_game.rb
+++ bowling/bowling_game.rb
@@ -2,11 +2,23 @@
 
 # BowlingGame class caluclates a game score out of indevidual roll inputs
 class BowlingGame
-  def initialize; end
+  def initialize
+    @attempts = []
+    @frames = []
+  end
 
   # no_of_pins is expected to be an integer between 0-10
   RANGE_ERR = 'no_of_pins out of expected range (0-10), was '
   def roll(no_of_pins)
     raise ArgumentError, RANGE_ERR + no_of_pins.to_s if no_of_pins.negative? || (no_of_pins > 10)
+
+    @attempts.push(no_of_pins)
+
+    @frames.push([])
+    @frames[0].push(no_of_pins)
+  end
+
+  def score
+    @frames[0].inject(0, :+)
   end
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -3,9 +3,8 @@
 require_relative '../bowling/bowling_game'
 
 describe BowlingGame do
-  bg = BowlingGame.new
-
   describe 'roll' do
+    bg = BowlingGame.new
     context 'Given a negative number of rolls' do
       it 'returns an exception' do
         expect do
@@ -27,6 +26,22 @@ describe BowlingGame do
             bg.roll(i)
           end
         end.not_to raise_error
+      end
+    end
+  end
+
+  describe 'score' do
+    context 'Given two rolls' do
+      it 'Return a score joining them' do
+        bg = BowlingGame.new
+        bg.roll(4)
+        bg.roll(4)
+        expect(bg.score).to eq(8)
+
+        bg = BowlingGame.new
+        bg.roll(2)
+        bg.roll(5)
+        expect(bg.score).to eq(7)
       end
     end
   end
